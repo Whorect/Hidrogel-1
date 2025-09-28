acc0b13a594e26fb705ffba302bd643e8ff9d8b5
Whorect <128117554+Whorect@users.noreply.github.com>
2025-09-28 16:29:42 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 import staticAdapter from '@astrojs/static';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://<nama-project>.pages.dev',
+  integrations: [sitemap()],
+  output: 'static',
   adapter: staticAdapter(),
-  outDir: './dist',
 });
