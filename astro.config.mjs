a6802d833d8df92c842889887dd989e21c67f715
Whorect <128117554+Whorect@users.noreply.github.com>
2025-09-28 16:22:32 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
+import staticAdapter from '@astrojs/static';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()]
-    },
-    integrations: [react()],
-    adapter: netlify()
+  adapter: staticAdapter(),
+  outDir: './dist',
 });
